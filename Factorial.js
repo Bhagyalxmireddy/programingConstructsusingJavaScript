ea90db890d5e31f0c8649e9dbbd3b1a9fa487b2c
Bhagi <puppyreddy1997@gmail.com>
2020-12-21 22:28:23 +0530

    [Bhagyalaxmi]ADDED:sloving The programs using Whileloop statements

--- Factorial.js
+++
@@ -1,7 +0,0 @@
-const readline = require('readline-sync');
-let number = readline.questionInt("Enter a number: ");
-let fact = 1;
-for (let i = 1; i <= number; i++) {
-	fact *= i;
-}
-console.log("Factorial of " + number + " is: " + fact);
